c8ded988e3ec04aa7639b71242a16d57e9c81f1f
govind-avvaru <98160328+govind-avvaru@users.noreply.github.com>
2022-09-03 07:32:50 +0530

    Update Orderplaceafter.php

--- Customerattribute/Observer/Orderplaceafter.php
+++ Customerattribute/Observer/Orderplaceafter.php
@@ -1,28 +1,38 @@
-<?php
-namespace I95dev\Customerattribute\Observer;
-use Magento\Framework\Event\ObserverInterface;
-use Magento\Customer\Model\Customer;
+<?php 
+namespace I95Dev\Customerattribute\Observer; 
+use Magento\Framework\Event\ObserverInterface; 
 
-class Orderplaceafter implements ObserverInterface
-{
+class Orderplaceafter implements ObserverInterface 
+
+{ 
+
+   protected $customerRepository; 
+
   
-    protected $_customerRepositoryInterface;
-  
 
-    public function __construct(\Magento\Customer\Api\CustomerRepositoryInterface $customerRepositoryInterface
-    )
-    {
-        $this->_customerRepositoryInterface = $customerRepositoryInterface;
-        
-    }
+    public function __construct( 
 
-    public function execute(\Magento\Framework\Event\Observer $observer)
-    {
-        $customer = $observer->getEvent()->getCustomer();
-        $customerData = $customer->getDataModel();
-        $customerData->setCustomAttribute('Last_ordered_date', (new \DateTime())->format(\Magento\Framework\Stdlib\DateTime::DATETIME_PHP_FORMAT));
-        $customer->updateData($customerData);
-        $customer->save();
+        \Magento\Customer\Api\CustomerRepositoryInterfaceFactory $customerRepositoryFactory)
+    {         
 
-    }
-}
+        $this->customerRepository = $customerRepositoryFactory->create(); 
+
+    } 
+
+    public function execute(\Magento\Framework\Event\Observer $observer) 
+
+    {    
+
+      $orderdata = $observer->getEvent()->getOrder(); 
+
+      $customerdata = $this->customerRepository->getById($ orderdata ->getData('customer_id')); 
+
+      $value= $orderdata ->getCreatedAt(); 
+
+      $customerdata ->setCustomAttribute('last_order_date', $value); 
+
+    $this->customerRepository->save($customerdata);  
+
+   } 
+
+} 
